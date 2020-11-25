45f9f26d7d1e906b1d79457dfac2b4703d35fec2
Ziheng Xie <ziheng.xie@shopee.com>
2020-11-25 19:37:16 +0800

    feat(golang/search): support goto field implementation

---
+++ src/main/kotlin/com/github/kkkiio/intellij/protobuf/support/golang/PbGoIdFilter.kt
@@ -0,0 +1,86 @@
+package com.github.kkkiio.intellij.protobuf.support.golang
+
+import com.goide.GoFileType
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileTypes.FileTypeRegistry
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.VirtualFileWithId
+import com.intellij.psi.util.CachedValue
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.util.SystemProperties
+import com.intellij.util.concurrency.NonUrgentExecutor
+import com.intellij.util.indexing.FileBasedIndex
+import com.intellij.util.indexing.IdFilter
+import java.util.concurrent.*
+
+
+private val log: Logger = Logger.getInstance(PbGolangStructImplementationsSearch::class.java)
+
+private val ID_FILTER_CALCULATION_TIMEOUT_MS = SystemProperties.getIntProperty("goland.id.filter.timeout.ms", 50)
+private val PB_GO_FILTER: Key<CachedValue<Future<Map<Int, String>>>> = Key.create("ALL_PB_GO")
+
+fun getPbGoIdFilter(project: Project): Map<Int, String>? {
+    val fileTypeRegistry = FileTypeRegistry.getInstance()
+    val filterCondition = { file: VirtualFile -> fileTypeRegistry.isFileOfType(file, GoFileType.INSTANCE) && file.name.endsWith(".pb.go") }
+    val key = PB_GO_FILTER
+    val future = CachedValuesManager.getManager(project)
+            .getCachedValue(project, key, {
+                val task = FutureTask<Map<Int, String>> {
+                    val idToFnMap = mutableMapOf<Int, String>()
+                    FileBasedIndex.getInstance()
+                            .iterateIndexableFiles({ fileOrDir: VirtualFile ->
+                                if (filterCondition(fileOrDir) && fileOrDir is VirtualFileWithId && fileOrDir.id >= 0) {
+                                    idToFnMap[fileOrDir.id] = fileOrDir.name
+                                }
+                                ProgressManager.checkCanceled()
+                                true
+                            }, project, null)
+                    idToFnMap
+                }
+                NonUrgentExecutor.getInstance().execute(task)
+                CachedValueProvider.Result.create(task, ProjectRootManager.getInstance(project), VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS)
+            }, false)
+    return try {
+        val idToFnMap = future.get(ID_FILTER_CALCULATION_TIMEOUT_MS.toLong(), TimeUnit.MILLISECONDS)
+        log.debug("IdFilter[$key]. Cache hit.")
+        idToFnMap
+    } catch (e: Exception) {
+        when (e) {
+            is ExecutionException, is TimeoutException, is InterruptedException -> {
+                log.debug("IdFilter[$key]. Cache miss. Filter will be calculated later")
+                null
+            }
+            else -> throw e
+        }
+    }
+}
+
+fun getAllPbGoFilesFilter(project: Project): IdFilter? {
+    return getPbGoIdFilter(project)?.let { PbGoIdFilter(it) }
+}
+
+fun getGenPbGoFileFilter(project: Project,
+                         protoFileBaseName: String): IdFilter? {
+    return getPbGoIdFilter(project)?.let {
+        GeneratedPbGoIdFilter(it, protoFileBaseName)
+    }
+}
+
+class GeneratedPbGoIdFilter(private val it: Map<Int, String>,
+                            private val protoFileBaseName: String) : IdFilter() {
+    override fun containsFileId(id: Int): Boolean {
+        return it.get(id) == "$protoFileBaseName.pb.go"
+    }
+}
+
+class PbGoIdFilter(private val idToFnMap: Map<Int, String>) : IdFilter() {
+    override fun containsFileId(id: Int): Boolean {
+        return idToFnMap.containsKey(id)
+    }
+}
--- src/main/kotlin/com/github/kkkiio/intellij/protobuf/support/golang/PbGolangStructImplementationsSearch.kt
+++ src/main/kotlin/com/github/kkkiio/intellij/protobuf/support/golang/PbGolangStructImplementationsSearch.kt
@@ -7,44 +7,76 @@ import com.goide.psi.GoTypeSpec
 import com.goide.stubs.index.GoTypesIndex
 import com.intellij.openapi.application.QueryExecutorBase
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.searches.DefinitionsScopedSearch.SearchParameters
+import com.intellij.psi.util.parentOfType
 import com.intellij.util.Processor
 import idea.plugin.protoeditor.lang.PbLanguage
+import idea.plugin.protoeditor.lang.psi.PbField
 import idea.plugin.protoeditor.lang.psi.PbMessageDefinition
 import org.jetbrains.annotations.NotNull
 
 
 private val log: Logger = Logger.getInstance(PbGolangStructImplementationsSearch::class.java)
 
-
 fun GlobalSearchScope.intersectIfNeeded(parameter: SearchParameters): GlobalSearchScope {
     val parameterScope = parameter.scope
     return if (parameter is WithScopeRestriction && parameterScope is GlobalSearchScope) intersectWith(parameterScope) else this
 }
 
-class PbGolangStructImplementationsSearch : QueryExecutorBase<GoSpecType, SearchParameters>(true) {
+class PbGolangStructImplementationsSearch : QueryExecutorBase<PsiElement, SearchParameters>(true) {
     override fun processQuery(queryParameters: @NotNull SearchParameters,
-                              consumer: @NotNull Processor<in GoSpecType>) {
+                              consumer: @NotNull Processor<in PsiElement>) {
         // todo: figure out why processQuery called twice
         log.info("processQuery start, queryParameters.element=${queryParameters.element}, consumer=$consumer")
-        val pbMessageDefinition = queryParameters.element.takeIf { it.language.`is`(PbLanguage.INSTANCE) } as? PbMessageDefinition
+        val pbElem = queryParameters.element.takeIf { it.language.`is`(PbLanguage.INSTANCE) }
                 ?: return
+        val (goStructTypeConsumer, pbMessageDefinition) = when (pbElem) {
+            is PbField -> {
+                val pbMessageDefinition = pbElem.parentOfType<PbMessageDefinition>()
+                if (pbMessageDefinition == null) {
+                    log.info("pbElem.parentOfType<PbMessageDefinition>()=null, pbElem=$pbElem")
+                    return
+                }
+                val pbFieldName = pbElem.name
+                if (pbFieldName == null) {
+                    log.info("pbElem.name=null, pbElem=$pbElem")
+                    return
+                }
+                val goFieldName = protoGenSpec.goCamelCase(pbFieldName)
+                ({ _: GoSpecType, structType: GoStructType ->
+                    consumer.process(structType.fieldDefinitions.first { it.name == goFieldName })
+                }) to pbMessageDefinition
+            }
+            is PbMessageDefinition -> {
+                ({ specType: GoSpecType, _: GoStructType -> consumer.process(specType) }) to pbElem
+            }
+            else -> return
+        }
+
         log.info("pbMessageDefinition={name=${pbMessageDefinition.name}, body=${pbMessageDefinition.body}}")
         val messageName = pbMessageDefinition.name ?: return
-        val key = protoGenSpec.goCamelCase(messageName)
-        log.info("GoTypesIndex.process start, key=$key")
+        val goStructName = protoGenSpec.goCamelCase(messageName)
+        val filter = pbMessageDefinition.containingFile.name.let {
+            if (it.endsWith(".proto")) {
+                getGenPbGoFileFilter(queryParameters.project, it.dropLast(6))
+            } else {
+                getAllPbGoFilesFilter(queryParameters.project)
+            }
+        }
+        log.info("GoTypesIndex.process start, name=$goStructName, filter=$filter")
         GoTypesIndex.process(
-                key,
+                goStructName,
                 queryParameters.project,
                 GlobalSearchScope.projectScope(queryParameters.project),
-                // todo: filter file by ${protoFilename}.pb.go
-                null) { typeSpec: GoTypeSpec ->
+                filter
+        ) { typeSpec: GoTypeSpec ->
             val identifier = typeSpec.identifier
             val specType = typeSpec.specType
             log.info("index process start, typeSpec={name=${typeSpec.name}, isTypeAlias=${typeSpec.isTypeAlias}}, shouldGoDeeper=${typeSpec.shouldGoDeeper()}, identifier={text=${identifier.text}}, specType={type=${specType.type}, identifier=${specType.identifier}}")
-            specType.takeIf { it.type is GoStructType }?.let {
-                consumer.process(it)
+            (specType.type as? GoStructType)?.let { structType ->
+                goStructTypeConsumer(specType, structType)
             } ?: true
         }
     }
