b52937b8e15764abad074c625d1bb1a077e70c91
kkkiio <xc130d210@outlook.com>
2021-05-09 14:17:17 +0800

    feat(proto/format): support align assignment in enum

--- src/test/kotlin/com/github/kkkiio/intellij/protobuf/proto/format/ProtoFormatterTest.kt
+++ src/test/kotlin/com/github/kkkiio/intellij/protobuf/proto/format/ProtoFormatterTest.kt
@@ -1,25 +1,42 @@
 package com.github.kkkiio.intellij.protobuf.proto.format
 
 import com.github.kkkiio.intellij.protobuf.test.TestCaseBase
+import com.intellij.application.options.CodeStyle
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.codeStyle.CodeStyleManager
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings
+import idea.plugin.protoeditor.lang.PbLanguage
 
 class ProtoFormatterTest : TestCaseBase() {
     fun testSimple() {
-        doTest()
+        configAndTest {
+            getCommonSettings().ALIGN_GROUP_FIELD_DECLARATIONS = true
+        }
     }
 
     fun testQualifiedName() {
-        doTest()
+        configAndTest {
+            getCommonSettings().ALIGN_GROUP_FIELD_DECLARATIONS = true
+        }
     }
 
     fun testRpc() {
-        doTest()
+        configAndTest {
+            getCommonSettings().ALIGN_GROUP_FIELD_DECLARATIONS = true
+        }
     }
 
-    private fun doTest(c: Char? = null) {
+    fun testEnum() {
+        configAndTest {
+            getCommonSettings().ALIGN_GROUP_FIELD_DECLARATIONS = true
+        }
+    }
+
+    private fun configAndTest(c: Char? = null,
+                              configureOptions: () -> Unit = {}) {
         val testName: String = getTestName(true)
         configureCaseFile("$testName.before.proto")
+        configureOptions()
         if (c == null) {
             WriteCommandAction.runWriteCommandAction(myFixture.project) {
                 CodeStyleManager.getInstance(project)
@@ -31,5 +48,8 @@ class ProtoFormatterTest : TestCaseBase() {
         val after = "$testName.after.proto"
         checkResultByCaseFile(after)
     }
+
+    private fun getCommonSettings(): CommonCodeStyleSettings =
+            CodeStyle.getLanguageSettings(myFixture.file, PbLanguage.INSTANCE)
 
 }
